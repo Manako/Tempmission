b96d65655a5a37b6aa6e74de6126ab7e058a5df6
pampas93 <abhijit.93@hotmail.com>
2018-01-27 18:48:39 -0500

    Goal prefab

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoalScript : MonoBehaviour {
+
+    public string Scene_name;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "player")
+        {
+
+            if(collision.transform.GetComponent<Collect_Bits>().bitCount == 8)
+            {
+                //Load next scene
+                SceneManager.LoadScene(Scene_name, LoadSceneMode.Additive);
+            }
+
+        }
+    }
+}
