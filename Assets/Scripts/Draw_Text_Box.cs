b57f955990fc33f35995fd26b62c6e35b1ff541e
Ehrbot <ehrenschindelar@gmail.com>
2018-01-27 13:33:57 -0500

    added dialogue box functionallity

---
+++ Assets/Scripts/Draw_Text_Box.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draw_Text_Box : MonoBehaviour
+{
+    public Texture tex;
+    public Texture p_tex;
+    public TextHandler parent;
+    public string message;
+    public int msg_speed;
+    public Rect usebox;
+    private int characters;
+    private int timer;
+    private GUIStyle style = new GUIStyle("label");
+    bool draw_prompt;
+	// Use this for initialization
+	void Start () {
+        characters = 0;
+        timer = 0;
+        draw_prompt = false;
+    }
+	
+	// OnGUI is called once per frame
+	void OnGUI () {
+        if (timer < msg_speed)
+            timer++;
+        else
+        {
+            if (characters < message.Length)
+                characters++;
+            else
+            {
+                parent.Finish();
+            }
+            timer = 0;
+        }
+        if (usebox != new Rect(0,0,0,0))
+        {
+            GUI.DrawTexture(usebox, tex);
+            GUI.Label(usebox, message.Substring(0, characters), style);
+
+            if(draw_prompt)
+                GUI.DrawTexture(new Rect(usebox.x + usebox.width - p_tex.width - 10, usebox.y + usebox.height - p_tex.height - 10, p_tex.width, p_tex.height), p_tex);
+        }
+        else
+        {
+            GUI.DrawTexture(usebox, tex);
+            GUI.Label(new Rect(10, 10, 100, 50), message.Substring(0, characters), style);
+            if (draw_prompt)
+                GUI.DrawTexture(new Rect(usebox.x + usebox.width - p_tex.width - 10, usebox.y + usebox.height - p_tex.height - 10, p_tex.width, p_tex.height), p_tex);
+        }
+    }
+    public void Skip()
+    {
+        characters = message.Length;
+        parent.Finish();
+    }
+    public void SetStyle(GUIStyle sty)
+    {
+        style = sty;
+    }
+    public void SetPrompt(bool bl)
+    {
+        draw_prompt = bl;
+    }
+}
---
+++ Assets/Scripts/TextHandler.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextHandler : MonoBehaviour {
+    public Vector2 position;
+    public Texture texture;
+    public Texture prompt_texture;
+    public string[] strings;
+    public GUIStyle style;
+
+    private int state;
+    // Use this for initialization
+    private const int STATIC = 0;
+    private const int DRAWING = 1;
+    private const int FINISHED = 2;
+    private const int STARTING = 3;
+    private Draw_Text_Box textbox;
+    private int currentstring;
+    private SpriteRenderer rend;
+    private Rect usebox;
+    private bool draw_prompt;
+    private int counter;
+    void Start () {
+        state = STARTING;
+        currentstring = 0;
+        rend = gameObject.GetComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        usebox = new Rect(position.x, position.y, texture.width, texture.height);
+        draw_prompt = false;
+        counter = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(state == STARTING)
+        {
+            if (currentstring < strings.Length)
+            {
+                //start drawing text
+                textbox = gameObject.AddComponent(typeof(Draw_Text_Box)) as Draw_Text_Box;
+                textbox.message = strings[currentstring];
+                textbox.msg_speed = 3;
+                textbox.parent = this;
+                textbox.SetStyle(style);
+                textbox.tex = texture;
+                textbox.p_tex = prompt_texture;
+                if (usebox == new Rect(0, 0, 0, 0))
+                {
+                    textbox.usebox = usebox;
+                }
+                else
+                {
+                    textbox.usebox = usebox;
+                }
+                state = DRAWING;
+                currentstring++;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if(state == DRAWING)
+        {
+            if (Input.GetKeyDown("a"))
+            {
+                textbox.Skip();
+                state = FINISHED;
+            }
+        }else if(state == FINISHED)
+        {
+            if (Input.GetKeyDown("a"))
+            {
+                Destroy(textbox);
+                state = STARTING;
+                textbox.SetPrompt(false);
+                draw_prompt = false;
+                counter = 0;
+            }
+            if (counter > 15)
+            {
+                draw_prompt = !draw_prompt;
+                textbox.SetPrompt(draw_prompt);
+                counter = 0;
+            }
+            else
+            {
+                counter++;
+            }
+        }
+	}
+
+    public void Finish()
+    {
+        state = FINISHED;
+    }
+}
