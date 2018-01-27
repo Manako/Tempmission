43c0f8ff6b144ccc040c91aaeda88fcd820fb9f0
JeremyRhoades <jeremyrhoades91@gmail.com>
2018-01-27 11:22:38 -0500

    Character, Damage, and Moving Platforms
    Character can move, jump, and die.
    
    Three additional scripts have been added as well. We should be able to add a script to any object for it to:
    
    1. Kill the player
    2. Move platforms along the X axis
    3. Move platforms along the Y axis

---
+++ Assets/Scripts/StraightXMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StraightXMovement : MonoBehaviour {
+    public float maxY = 100;
+    public float minY = 0;
+
+    float move;
+    bool movingRight;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x > maxY)
+        {
+            movingRight = false;
+        }
+        else if (transform.position.x < minY)
+        {
+            movingRight = true;
+        }
+
+        if (movingRight)
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector2.left * Time.deltaTime);
+        }
+    }
+
+}
