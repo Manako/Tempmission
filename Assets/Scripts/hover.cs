b900cac51025f06b61f1cc4b561509234424cd5b
Ehrbot <ehrenschindelar@gmail.com>
2018-01-27 22:52:25 -0500

    hover

---
+++ Assets/Scripts/hover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hover : MonoBehaviour {
+    private float deg;
+	// Use this for initialization
+	void Start () {
+        deg = Random.value * 360;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        deg += 1;
+        gameObject.transform.Translate(Vector3.up * Mathf.Sin(deg/50) / 240);
+    }
+}
